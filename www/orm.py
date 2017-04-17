0f0b2bb93923a08f7a02f838b8a67bd2fcfa69bc
emicos <shuaiqixupeng@outlook.com>
2017-04-17 10:56:36 +0800

    fix annotation

--- www/orm.py
+++ www/orm.py
@@ -121,7 +121,7 @@ class ModelMetaclass(type):
 		attrs['__mappings__'] = mappings # 保存属性和列的映射关系
 		attrs['__table__'] = tableName
 		attrs['__primary_key__'] = primaryKey # 主键属性名
-		attrs['__fields__'] = fields # 除逐渐外的属性名
+		attrs['__fields__'] = fields # 除主键外的属性名
 		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
 		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
