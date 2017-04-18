812895112cc1d1ebab1f93cf18044a9b201f90f1
emicos <shuaiqixupeng@outlook.com>
2017-04-18 22:56:43 +0800

    Add MVC

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'RanHoNg'
+
+'''
+default configuration
+'''
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'www-data',
+		'password': 'www-data',
+		'database': 'awesome'
+	},
+	'session': {
+		'secret':'AwEsOmE'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'RanHoNg'
+'''
+override configuration
+'''
+
+configs = {
+	'db': {
+		'host': '192.168.0.100'
+	}
+}
